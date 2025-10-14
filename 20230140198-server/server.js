f50d4949941e84c8b48fd7a67e7d24729e352330
Latfiansya Dzikri - 198 <latfiansyadzikri@gmail.com>
2025-10-14 14:16:38 +0700

    Tugas PAW

---
+++ 20230140198-server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = 5000;
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Hello from Node.js Server!' });
+});
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`);
+});
