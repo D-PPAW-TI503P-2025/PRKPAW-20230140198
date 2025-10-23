616ce234a5b28bec12d71bf22afb48773b8a6811
Latfiansya Dzikri - 198 <latfiansyadzikri@gmail.com>
2025-10-23 15:50:33 +0700

    prk_paw4

--- 20230140198-server/middleware/permissionMiddleware.js
+++ 20230140198-server/middleware/permissionMiddleware.js
@@ -3,8 +3,8 @@ exports.addUserData = (req, res, next) => {
     console.log('Middleware: Menambahkan data user dummy...');
     req.user = {
         id: 123,
-        nama: 'User Karyawan', // ✅ perbaikan tanda kutip
-        role: 'karyawan'       // ubah ke 'admin' kalau mau tes route report
+        nama: 'User Karyawan', 
+        role: 'admin'       // ubah ke 'admin' kalau mau tes route report
     };
     next();
 };
